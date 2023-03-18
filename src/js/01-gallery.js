d14a28100e575bdc79986d4a20deef99985b3179
ValeriiHol <rielty@gmail.com>
2023-03-18 17:03:54 +0200

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery')
+const elements = []
+
+galleryItems.forEach(item => {
+	const galleryItem = `
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}"
+    alt="${item.description}"/>
+    </a>`;
+
+	elements.push(galleryItem);	
+})
+// console.log(elements);
+// console.log(elements.join(''));
+// gallery.insertAdjacentHTML('beforeend', elements);
+gallery.insertAdjacentHTML('beforeend', elements.join(''));
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250,
+	captionsData: 'alt',
+})
