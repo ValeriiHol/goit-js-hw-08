aab945bd30da06d7f4808b2302ee4ff5a6d71f73
ValeriiHol <rielty@gmail.com>
2023-03-19 15:19:51 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+// before 
+// npm i --save lodash.throttle
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData(event) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert(`Всі поля повинні бути заповненні.`);
+  }
+  localStorage.removeItem('feedback-form-state');
+  e.currentTarget.reset();
+  dataForm = {};
+}
